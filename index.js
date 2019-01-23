2001f75638bc86d6bef40cfa23db85c441f4345c
adrioro <artistsvillage@me.com>
2019-01-23 14:57:06 -0500

    Third commit

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+//To create a focus event in jQuery, we write code like this:
+
 $("textarea").focus(function () {
     $("textarea#textId").css("display", "none");
+});
+
+//Telling the browser to automatically focus on something.
+
+$("#focusHere").click(function () {
+    $("#inputFocus").focus();
+});
+
+//To blur is to unfocus. It is also used on elements that can be activated (or selected.)
+
+$("#inputThenBlur").focus(function () {
+    $(this).blur();
 });
